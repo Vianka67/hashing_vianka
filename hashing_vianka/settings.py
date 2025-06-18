7f301efc830d33f19ca7f83bb5ef63530f1c2143
Vianka67 <viankavacaflores86@gmail>
2025-06-18 19:49:41 -0400

    Actualización de archivos y mejoras

--- hashing_vianka/settings.py
+++ hashing_vianka/settings.py
@@ -120,3 +120,51 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# hashing_project/settings.py
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'hashing_app',
+    'rest_framework',
+    'corsheaders',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# ... otras configuraciones ...
+
+# Configuración de la base de datos MySQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hashing_db',      # Nombre de tu base de datos MySQL
+        'USER': 'root',            # Tu usuario de MySQL
+        'PASSWORD': 'your_mysql_password', # Tu contraseña de MySQL
+        'HOST': '127.0.0.1',       # O 'localhost'
+        'PORT': '3306',            # Puerto por defecto de MySQL
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
+    }
+}
+
+# Configuración de CORS (ajusta según tus necesidades)
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
